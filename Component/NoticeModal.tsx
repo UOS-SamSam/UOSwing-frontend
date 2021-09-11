382633aee717eb8a27cebf1068c8be9f0b2d5c14
Ressi <37998734+gmldms784@users.noreply.github.com>
2021-09-11 13:59:16 +0900

    api path 변경

--- Component/NoticeModal.tsx
+++ Component/NoticeModal.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
 
 import {
-	View
+	StyleSheet,
+	View,
+	Text,
+	Image,
+	Linking
 } from 'react-native';
+import { borderColor } from '../CommonVariable';
 import { NoticeScreen } from '../Screen';
 
 const NoticeModal = () => {
+	const gotoInstagram = () => {
+		Linking.openURL('https://instagram.com/uos_nalgae?utm_medium=copy_link');
+	}
+
 	return (
-		<View style={{width:'100%', maxHeight: '94%', marginTop: 20}}>
+		<View style={{width:'100%', maxHeight: '90%', marginTop: 20}}>
 			<NoticeScreen type="modal" />
+			<View style={ModalStyle.Recommend}>
+				<Text style={{padding: 2}}>날개의 활동이 궁금하다면? 👉</Text>
+				<View onTouchStart={gotoInstagram}>
+					<Image
+						style={{width: 40, height: 40}}
+						source={require('../assets/img/logo.png')}
+					/>
+				</View>
+			</View>
 		</View>
 	);
 }
+
+const ModalStyle = StyleSheet.create({
+	Recommend: {
+		height: '10%',
+		borderColor: borderColor,
+		borderWidth: 1,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+	}
+});
 
 export default NoticeModal;
